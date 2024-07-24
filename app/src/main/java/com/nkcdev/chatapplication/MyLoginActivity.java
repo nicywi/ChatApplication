8a41acde730daf5016202f0a40fe229d32866c46
Nikola Cywinska <nicywi@gmail.com>
2024-07-24 10:35:52 +0200

    my login functionality done

--- app/src/main/java/com/nkcdev/chatapplication/MyLoginActivity.java
+++ app/src/main/java/com/nkcdev/chatapplication/MyLoginActivity.java
@@ -1,22 +1,32 @@
 package com.nkcdev.chatapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MyLoginActivity extends AppCompatActivity {
 
     private TextInputEditText editTextEmail, editTextPassword;
     private Button buttonSignin, buttonSignup;
     private TextView textViewForgot;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +44,50 @@ public class MyLoginActivity extends AppCompatActivity {
         buttonSignin = findViewById(R.id.buttonSignin);
         buttonSignup = findViewById(R.id.buttonSignup);
         textViewForgot = findViewById(R.id.textViewForgot);
+        auth = FirebaseAuth.getInstance();
 
+        buttonSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                if (!email.equals("") && !password.equals("")){
+                    signIn(email, password);
+                }else{
+                    Toast.makeText(MyLoginActivity.this, "Email or password not correct", Toast.LENGTH_LONG);
+                }
+            }
+        });
 
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MyLoginActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
 
+        textViewForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MyLoginActivity.this, ForgotActivity.class);
+                startActivity(i);
+            }
+        });
+    }
 
+    public void signIn(String email, String password){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Intent i = new Intent(MyLoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                    Toast.makeText(MyLoginActivity.this, "Sign in is successful", Toast.LENGTH_LONG);
+                }else {
+                    Toast.makeText(MyLoginActivity.this, "Sign in is not successful", Toast.LENGTH_LONG);
+                }
+            }
+        });
     }
 }
